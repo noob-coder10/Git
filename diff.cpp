93f96c7ccc300439e9e3890ea37d5f2c3a7d7d22
Sayan Paul <sayanpaul2016@gmail.com>
2021-06-01 20:32:59 +0530

    updated

---
+++ diff.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<algorithm>
+#include<string>
+#define mod 1000000007
+#define ll long long int
+using namespace std;
+int main()
+{
+    int a=10,b=5;
+    cout<<a/b;
+    return 0;
+}
